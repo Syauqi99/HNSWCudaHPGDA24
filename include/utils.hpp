6dcb6cbab2a26b5aea32294dab570fd5ea0a138e
Syauqi99 <syauqirp@DESKTOP-3B6E5MD>
2024-11-20 21:19:35 +0100

    comment on utils

--- include/utils.hpp
+++ include/utils.hpp
@@ -15,39 +15,55 @@ using namespace std;
 namespace utils {
 
     template <typename T = float>
+    // data constructor
     struct Data {
+        // id of the data (in this case this is like the row)
         size_t id;
+        // the set of data in float
         std::vector<T> x;
 
+        // if nothing id =0, vector is {0}
         Data() : id(0), x({0}) {}
 
         Data(size_t i, std::vector<T> v) {
-            id = i;
+            id = i; // set id equal to the input
+            // copy the vectors v to x
             std::copy(v.begin(), v.end(), std::back_inserter(x));
         }
 
         Data(std::vector<T> v) {
+            // if no id then set id = 0
             id = 0;
+            // copy the vector only
             std::copy(v.begin(), v.end(), std::back_inserter(x));
         }
 
-        auto& operator [] (size_t i) { return x[i]; }
+        // return the value without copy
+        auto& operator [] (size_t i) { return x[i];}
+
+        // return a copy
         const auto& operator [] (size_t i) const { return x[i]; }
 
+        // define the comparison between data
         bool operator==(const Data &o) const {
+            // they only compare the id
             if (id == o.id) return true;
             return false;
         }
-
+        // same as before but the opposite
         bool operator!=(const Data &o) const {
             if (id != o.id) return true;
             return false;
         }
 
+        // return size
         size_t size() const { return x.size(); }
+
+        // iterator
         auto begin() const { return x.begin(); }
         auto end() const { return x.end(); }
 
+        // print the data
         void show() const {
             std::cout << id << ": ";
             for (const auto &xi : x) {
@@ -56,13 +72,18 @@ namespace utils {
             std::cout << std::endl;
         }
     };
-
+    
+    // template is an alias so if the type is not specified then it will float. Otherwise, it follows the one that they specify
     template <typename T = float>
+    // Dataset is the type alias so we can use dataset right away
     using Dataset = vector<Data<T>>;
-
     template <typename T = float>
     using DistanceFunction = function<float(Data<T>, Data<T>)>;
+    // this function wrapper that return float, with two parameters
+    // abstract functions
 
+    // define the euclidian distance
+    // optimizes with CUDA
     template <typename T = float>
     auto euclidean_distance(const Data<T>& p1, const Data<T>& p2) {
         float result = 0;
@@ -73,9 +94,11 @@ namespace utils {
         return result;
     }
 
+    // reading the data from files
     Dataset<float> fvecs_read(const std::string& filename, int size) {
+        // filename input constructor to read the data
         std::ifstream file(filename, std::ios::binary);
-        
+        // there file not found or the data is corrupted return an error
         if (!file.is_open()) {
             throw std::runtime_error("I/O error: Unable to open the file " + filename);
         }
@@ -125,6 +148,7 @@ namespace utils {
         return dataset;
     }
 
+    // count the running time of the code 
     auto get_now() { return chrono::system_clock::now(); }
 
     auto get_duration(chrono::system_clock::time_point start,
@@ -132,35 +156,43 @@ namespace utils {
         return chrono::duration_cast<chrono::microseconds>(end - start).count();
     }
 
+    // numeric limit
     constexpr auto double_max = numeric_limits<double>::max();
     constexpr auto double_min = numeric_limits<double>::min();
 
     constexpr auto float_max = numeric_limits<float>::max();
     constexpr auto float_min = numeric_limits<float>::min();
-
+    // constructor of the neighbor of each nodes
     struct Neighbor {
         float dist;
         int id;
 
+        // optimizes by cuda
         Neighbor() : dist(float_max), id(-1) {}
         Neighbor(float dist, int id) : dist(dist), id(id) {}
     };
 
     using Neighbors = vector<Neighbor>;
 
+    // for making comparison between two neighbor by distance gives true if less
     struct CompLess {
         constexpr bool operator()(const Neighbor& n1, const Neighbor& n2) const noexcept {
             return n1.dist < n2.dist;
         }
     };
 
+    // for making comparison between two neighbor by distance gives true if more
     struct CompGreater {
         constexpr bool operator()(const Neighbor& n1, const Neighbor& n2) const noexcept {
             return n1.dist > n2.dist;
         }
     };
 
+    // for making comparison between two neighbor by distance gives true if less
     template <typename T>
+
+    // knn linear search 
+    // optimizes with CUDA
     auto scan_knn_search(const Data<T>& query, int k, const Dataset<T>& dataset) {
         const auto df = euclidean_distance<float>;
         auto threshold = float_max;
