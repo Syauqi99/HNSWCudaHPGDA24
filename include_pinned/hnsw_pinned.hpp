afce1b72f2348693ad595041f982f1667f483ee4
juanmigutierrez <recuperacionmiguel@outlook.es>
2025-01-01 18:00:44 +0100

    fixing

--- include_pinned/hnsw_pinned.hpp
+++ include_pinned/hnsw_pinned.hpp
@@ -291,7 +291,6 @@ namespace hnsw {
                         calculateDistances<<<numBlocks, blockSize, 0, streams[s]>>>(
                             d_query_buffer,
                             d_neighbor_buffer,
-                            d_batch_indices,
                             d_distances_buffer + startIdx,
                             query.x.size(),
                             endIdx - startIdx
